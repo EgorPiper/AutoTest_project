8301e0bd38ca19fcd063534f47b410b9bb2ac322
EgorPiper <48902908+EgorPiper@users.noreply.github.com>
2020-06-26 11:13:49 +0400

    finally project

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,33 @@
-def test_guest_can_go_to_login_page(browser):
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+
+
+def test_guest_can_do_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_enter_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.is_basket_empty()
+    basket_page.should_be_basket_empty_message()
